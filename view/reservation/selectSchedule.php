7b881fc5f84cdd06c7ea77ef053e9b98d580014f
Santiago Gómez <santii810@hotmail.com>
2018-11-14 23:45:35 +0100

    Update selectSchedule.php
    Corregido error en la vista

--- view/reservation/selectSchedule.php
+++ view/reservation/selectSchedule.php
@@ -15,11 +15,7 @@ $buttonColour = array("btn-success","btn-success","btn-success", "btn-success", 
 function getNumReservations($date, $hour, $reservations){
   $toret = 0;
   foreach ($reservations as $reservation) {
-    //echo $reservation["fecha"];
-    //echo date("Y-m-d", $date);
-    if($reservation["fecha"] == date("Y-m-d", $date)
-      && $reservation["hora"] == $hour.":00")
-
+    if($reservation["fecha"] == date("Y-m-d", $date) && substr($reservation["hora"],0,5) == $hour)
       return $reservation["numReservas"];
   }
   return $toret;
@@ -41,7 +37,7 @@ function getNumReservations($date, $hour, $reservations){
       </div>
       <div class="col-md-3 col-sm-5" >
         <button type="button" class="btn btn-warning btn-xs" disabled>
-          Solo 1 plaza 
+          Ultima pista 
         </button>
       </div>
       <div class="col-md-2 col-sm-3" >
@@ -60,40 +56,38 @@ function getNumReservations($date, $hour, $reservations){
     <div class="container">
       <?php    $date = time();
       for ($i = 0; $i < 7; $i++) { ?>
-      <div class="row">
-        <div class="col-xl-12">
-          <?php echo "<br><h3>". date('d-m-Y', $date). "</h3>";
-          ?>
+        <div class="row">
+          <div class="col-xl-12">
+            <?php echo "<br><h3>". date('d-m-Y', $date). "</h3>";
+            ?>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <?php foreach ($hours as $hour) { 
-          $numReservas = getNumReservations($date, $hour, $reservations);
-          ?>
-          <div class="col-sm-3 col-6" >
-            <div class="container">
-              <div class="row">
-                <div class="col-xl-10">
+        <div class="row">
+          <?php foreach ($hours as $hour) { 
+            $numReservas = getNumReservations($date, $hour, $reservations);
+            ?>
+            <div class="col-sm-3 col-6" >
+              <div class="container">
+                <div class="row">
+                  <div class="col-xl-10">
+                  </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-xl-10">
+                <div class="row">
+                  <div class="col-xl-10">
 
-                  <button type="submit"
-                  name="scheduleButton"
-                  value="<?php echo date('d-m-Y', $date)."#".$hour?>"
-                  class="btn <?php echo $buttonColour[$numReservas] ?> btn-xs"
-                  <?php if($numReservas>=5) echo "disabled" ?>>
-                  <?php echo $hour?> 
-                </button>
+                    <button type="submit"
+                    name="scheduleButton"
+                    value="<?php echo date('d-m-Y', $date)."#".$hour?>"
+                    class="btn <?php echo $buttonColour[$numReservas] ?> btn-xs"
+                    <?php if($numReservas>=5) echo "disabled" ?>> 
+                    <?php echo $hour?> 
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-
-        </div>
         <?php }?>
       </div> 
-
       <?php $date = strtotime('tomorrow', $date); }?>
     </div>
   </form>

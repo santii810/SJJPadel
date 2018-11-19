4e16845570c22cdefbd42257c36125164d0a6139
Santiago Gómez <santii810@hotmail.com>
2018-11-19 05:30:52 +0100

    Borrado footer

--- view/layouts/default.php
+++ view/layouts/default.php
@@ -187,56 +187,24 @@ $currentRol = $view->getVariable("currentRol");
 
 				<?= $view->getFragment(ViewManager::DEFAULT_FRAGMENT) ?>
 			</div>
-		</div> <!-- /container -->
+		</div> 
 
 		<footer id="myFooter">
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-3 myCols">
-						<h5>Get started</h5>
-						<ul>
-							<li><a href="#">Home</a></li>
-							<li><a href="#">Sign up</a></li>
-							<li><a href="#">Downloads</a></li>
-						</ul>
-					</div>
-					<div class="col-sm-3 myCols">
-						<h5>About us</h5>
-						<ul>
-							<li><a href="#">Company Information</a></li>
-							<li><a href="#">Contact us</a></li>
-							<li><a href="#">Reviews</a></li>
-						</ul>
-					</div>
-					<div class="col-sm-3 myCols">
-						<h5>Support</h5>
-						<ul>
-							<li><a href="#">FAQ</a></li>
-							<li><a href="#">Help desk</a></li>
-							<li><a href="#">Forums</a></li>
-						</ul>
-					</div>
-					<div class="col-sm-3 myCols">
-						<h5>Legal</h5>
-						<ul>
-							<li><a href="#">Terms of Service</a></li>
-							<li><a href="#">Terms of Use</a></li>
-							<li><a href="#">Privacy Policy</a></li>
-						</ul>
+						
 					</div>
 				</div>
-			</div>
-			<div class="social-networks">
-				<a href="https://twitter.com/?lang=es" class="twitter"><i class="fa fa-twitter"></i></a>
-				<a href="#" class="facebook"><i class="fa fa-facebook-official"></i></a>
-				<a href="#" class="google"><i class="fa fa-google-plus"></i></a>
-			</div>
-			<div class="footer-copyright">
-				<p>© 2019 SJJPadel Company </p>
-			</div>
-		</footer>
+				<div class="social-networks">
+
+				</div>
+				<div class="footer-copyright">
+					<p>© 2018 SJJPadel Company </p>
+				</div>
+			</footer>
 
 
 
-	</body>
-	</html>
+		</body>
+		</html>

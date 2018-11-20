f00c0057e2b9aebf8cce51e0967701d1e27e85c4
Santiago Gómez <santii810@hotmail.com>
2018-11-20 18:36:21 +0100

    Modificado horario de fecha y de hora en showConfrontation

--- view/confrontation/showConfrontations.php
+++ view/confrontation/showConfrontations.php
@@ -1,53 +1,64 @@
 <?php
-//file: view/confrontation/selectGroup.php
-
-require_once(__DIR__."/../../core/ViewManager.php");
+// file: view/confrontation/selectGroup.php
+require_once (__DIR__ . "/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
 
 $errors = $view->getVariable("errors");
 $confrontations = $view->getVariable("confrontations");
 $view->setVariable("title", i18n("Enfrentamientos"));
-$cabecera = array("Pareja1","Pareja2","Fecha","Hora","Sets Pareja1", "Sets Pareja2");
+$cabecera = array(
+    "Pareja1",
+    "Pareja2",
+    "Fecha",
+    "Hora",
+    "Sets Pareja1",
+    "Sets Pareja2"
+);
 ?>
 
 <h3><?= i18n("Enfrentamientos")?></h3>
 
-<form action="index.php?controller=confrontation&amp;action=setresults" method="POST">
+<form action="index.php?controller=confrontation&amp;action=setresults"
+	method="POST">
 
-  <table class="table table-striped">
-  <thead>
+	<table class="table table-striped">
+		<thead class="thead-dark">
     <?php foreach($cabecera as $valor) { ?>
         <th scope="col">
           <?php echo $valor; ?>
         </th>
       <?php } ?>
   </thead>
-  <tbody>
+		<tbody>
    <?php foreach($confrontations as $confrontation) { ?>
       <tr>
-        <td scope="col">
+				<td scope="col">
           <?php echo $confrontation->getPartner1Names(); ?>
         </td>
-        <td scope="col">
+				<td scope="col">
           <?php echo $confrontation->getPartner2Names(); ?>
         </td>
-        <td scope="col">
-          <?php echo $confrontation->getDate(); ?>
+				<td scope="col">
+          <?php
+    if ($confrontation->getDate() != null) {
+        echo date_format(DateTime::createFromFormat("Y-m-d", $confrontation->getDate()), "d-m-Y");
+    }
+    ?>
         </td>
-        <td scope="col">
-          <?php echo $confrontation->getTime(); ?>
+				<td scope="col">
+          <?php echo substr($confrontation->getTime(), 0,5); ?>
         </td>
-        <td scope="col">
+				<td scope="col">
           <?php echo $confrontation->getSetsPartner1(); ?>
         </td>
-        <td scope="col">
+				<td scope="col">
           <?php echo $confrontation->getSetsPartner2(); ?>
         </td>
-      </tr>
+			</tr>
       <?php } ?>
   </tbody>
-</table>
+	</table>
 
-<input type="hidden" name="idGrupo" value="<?php echo $idGrupo ?>">
+	<input type="hidden" name="idGrupo" value="<?php echo $idGrupo ?>">
 
 </form>

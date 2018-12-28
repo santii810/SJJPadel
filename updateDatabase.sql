bd3c276a1ad14f70878cadba2e25b381a4ae5a31
santii810 <santii810@hotmail.com>
2018-12-28 19:58:13 +0100

    Actualizada base de datos con los campos fase corregidos

--- updateDatabase.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `campeonato` ADD `fase` ENUM('Inscripcion', 'Grupos', 'Eliminatoria') NOT NULL AFTER `nombreCampeonato`;
-ALTER TABLE `enfrentamiento` ADD `fase` ENUM('Grupos', 'Cuartos', 'Semifinal', 'Final') NOT NULL AFTER `idGrupo`;
